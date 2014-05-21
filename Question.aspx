6ef6aa89da498854cecfd8f5da592ee6f8a78a42
Rogerio <rogerio16jr@hotmail.com>
2014-05-20 23:20:15 -0400

    First Commit

---
+++ Question.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Question.aspx.cs" Inherits="StudyWords.Question" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        .boxPrincipal {
+            border: 0px solid;
+            padding: 15px;
+            background: #fff;
+            border-collapse: separate;
+            width: 400px;
+            height: 150px;
+        }
+
+        .ckOption {
+            cursor: pointer;
+        }
+        .boxCheck {
+            float:right;
+            width: 50%;
+        }
+
+        .button {
+            width: 100px;
+            text-align: center;
+        }
+
+        .btNext {
+            float: right;
+        }
+
+        .btPrevious {
+            float: left;
+        }
+
+        body {
+            background: #f1f4f9;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btLoadQuestion" runat="server" Text="Load Question" OnClick="OnClick_btLoadQuestion" />
+        </div>
+        <div class="boxPrincipal">
+
+            <fieldset id="fdsQuestion">
+                <legend>Question</legend>
+                <div id="divQuestion" runat="server" class="BoxQuestion">
+                    <asp:Label ID="lbQuestion" runat="server" ></asp:Label>
+                </div>
+            </fieldset>
+
+            <fieldset id="fdsOption">
+                <legend>Option</legend>
+                <div id="divOption" runat="server" class="BoxOption">
+                    <div class="boxCheck"> <asp:RadioButton ID="rb1Text" runat="server" Text="be" ToolTip="be" GroupName="Group" CssClass="ckOption"  /></div>
+                    <div class="boxCheck"> <asp:RadioButton ID="rb2Text" runat="server" Text="we/was" ToolTip="we/was" GroupName="Group" CssClass="ckOption" /></div>
+                    <div class="boxCheck"> <asp:RadioButton ID="rb3Text" runat="server" Text="been" ToolTip="been" GroupName="Group" CssClass="ckOption" /></div>
+                    <div class="boxCheck"> <asp:RadioButton ID="rb4Text" runat="server" Text="was" ToolTip="was" GroupName="Group" CssClass="ckOption" /></div>
+                </div>
+            </fieldset>
+
+            <div class="btPrevious">
+                <asp:Button ID="btPrevious" runat="server" Text="Previous" CssClass="button" OnClick="btPrevious_OnClick" />
+            </div>
+            <div class="btNext">
+                <asp:Button ID="btNext" runat="server" Text="Next" CssClass="button" OnClick="btNext_OnClick" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Question.aspx.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StudyWords
+{
+    public partial class Question : System.Web.UI.Page
+    {
+        ManagerQuestion _objManager;
+        private ManagerQuestion glbManagerQuestion
+        {
+            get
+            {
+                if (_objManager == null)
+                {
+                    _objManager = (ManagerQuestion)Session["ManagerQuestion"];
+                }
+
+                return _objManager;
+            }
+
+            set
+            {
+                _objManager = value;
+                Session.Remove("ManagerQuestion");
+                Session["ManagerQuestion"] = _objManager;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void OnClick_btLoadQuestion(object sender, EventArgs e)
+        {
+            string sMessage = string.Empty;
+            ManagerQuestion objManager = new ManagerQuestion();
+            objManager.GenerateQuestion(out sMessage);
+
+            glbManagerQuestion = objManager;
+
+            QuestionClass objQuestionClass = glbManagerQuestion.GetCurrentQuestion(out sMessage);
+            FillScreen(objQuestionClass, out sMessage);
+        }
+
+        protected void btNext_OnClick(object sender, EventArgs e)
+        {
+            string sMessage = string.Empty;
+            QuestionClass objQuestionClass = new QuestionClass();
+
+            objQuestionClass = glbManagerQuestion.GetCurrentQuestion(out sMessage);
+            GetDataScreen(objQuestionClass, out sMessage);
+
+            objQuestionClass = glbManagerQuestion.NextQuestion(out sMessage);
+            FillScreen(objQuestionClass, out sMessage);
+        }
+
+        protected void btPrevious_OnClick(object sender, EventArgs e)
+        {
+            string sMessage = string.Empty;
+            QuestionClass objQuestionClass = new QuestionClass();
+
+            objQuestionClass = glbManagerQuestion.GetCurrentQuestion(out sMessage);
+            GetDataScreen(objQuestionClass, out sMessage);
+
+            objQuestionClass = glbManagerQuestion.PreviousQuestion(out sMessage);
+            FillScreen(objQuestionClass, out sMessage);
+        }
+
+        private void FillScreen(QuestionClass pQuestion, out string sMessage)
+        {
+            sMessage = string.Empty;
+            try
+            {
+                lbQuestion.Text = pQuestion.Description;
+
+                rb1Text.Text = pQuestion.ListOption[0].Description;
+                rb1Text.Checked = pQuestion.ListOption[0].Cheecked;
+
+                rb2Text.Text = pQuestion.ListOption[1].Description;
+                rb2Text.Checked = pQuestion.ListOption[1].Cheecked;
+
+                rb3Text.Text = pQuestion.ListOption[2].Description;
+                rb3Text.Checked = pQuestion.ListOption[2].Cheecked;
+
+                rb4Text.Text = pQuestion.ListOption[3].Description;
+                rb4Text.Checked = pQuestion.ListOption[3].Cheecked;
+
+            }
+            catch (Exception ex)
+            {
+                sMessage = "Error unexpected! Exception[" + ex.Message + "]";
+            }
+        }
+        private void GetDataScreen(QuestionClass pQuestion, out string sMessage)
+        {
+            sMessage = string.Empty;
+            try
+            {
+                pQuestion.ListOption[0].Cheecked = rb1Text.Checked;
+                pQuestion.ListOption[1].Cheecked = rb2Text.Checked;
+                pQuestion.ListOption[2].Cheecked = rb3Text.Checked;
+                pQuestion.ListOption[3].Cheecked = rb4Text.Checked;
+            }
+            catch (Exception ex)
+            {
+                sMessage = "Erro unexpected! Exception[" + ex.Message + "]";
+            }
+        }
+
+        class ManagerQuestion
+        {
+            private int iIndexQuestion;
+            private int iTotalQuestion;
+            public List<QuestionClass> ListQuestion { get; set; }
+            public void GenerateQuestion(out string sMessage)
+            {
+                sMessage = string.Empty;
+                try
+                {
+                    QuestionClass objQuestion = new QuestionClass();
+                    objQuestion.Id = 1;
+                    objQuestion.Description = "Simple Past";
+
+                    Option objOption1 = new Option();
+                    objOption1.Id = 1;
+                    objOption1.Description = "be";
+
+                    Option objOption2 = new Option();
+                    objOption2.Id = 2;
+                    objOption2.Description = "was/were";
+
+                    Option objOption3 = new Option();
+                    objOption3.Id = 3;
+                    objOption3.Description = "been";
+
+                    objQuestion.ListOption.Add(objOption1);
+                    objQuestion.ListOption.Add(objOption2);
+                    objQuestion.ListOption.Add(objOption3);
+
+                    this.ListQuestion.Add(objQuestion);
+                    iTotalQuestion++;
+
+                    objQuestion.RandomOption(out sMessage);
+
+                    objQuestion = new QuestionClass();
+                    objQuestion.Id = 2;
+                    objQuestion.Description = "Simple Past";
+
+                    objOption1 = new Option();
+                    objOption1.Id = 4;
+                    objOption1.Description = "dig";
+
+                    objOption2 = new Option();
+                    objOption2.Id = 5;
+                    objOption2.Description = "dug";
+
+                    objOption3 = new Option();
+                    objOption3.Id = 6;
+                    objOption3.Description = "done";
+
+                    objQuestion.ListOption.Add(objOption1);
+                    objQuestion.ListOption.Add(objOption2);
+                    objQuestion.ListOption.Add(objOption3);
+
+                    objQuestion.RandomOption(out sMessage);
+
+                    this.ListQuestion.Add(objQuestion);
+                    iTotalQuestion++;
+                }
+                catch (Exception ex)
+                {
+                    sMessage = "Erro unespected [" + ex.Message + "]";
+                }
+            }
+            public ManagerQuestion()
+            {
+                this.ListQuestion = new List<QuestionClass>();
+                iIndexQuestion = 0;
+            }
+            public QuestionClass NextQuestion(out string sMessage)
+            {
+                sMessage = string.Empty;
+                QuestionClass objQuestion = new QuestionClass();
+                try
+                {
+                    iIndexQuestion++;
+                    if (iIndexQuestion < iTotalQuestion)
+                    {
+                        objQuestion = ListQuestion[iIndexQuestion];
+                    }
+                    else if( ListQuestion.Count > 0)
+                    {
+                        iIndexQuestion = 0;
+                        objQuestion = ListQuestion[0];
+                    }
+                }
+                catch (Exception ex)
+                {
+                    sMessage = "Erro unexpected! Exception[" + ex.Message + "]";
+                }
+
+                return objQuestion;
+            }
+            public QuestionClass PreviousQuestion(out string sMessage)
+            {
+                sMessage = string.Empty;
+                QuestionClass objQuestion = new QuestionClass();
+                try
+                {
+                    iIndexQuestion--;
+                    if (iIndexQuestion > 0)
+                    {
+                        objQuestion = ListQuestion[iIndexQuestion];
+                    }
+                    else if (ListQuestion.Count > 0)
+                    {
+                        iIndexQuestion = 0;
+                        objQuestion = ListQuestion[0];
+                    }
+                }
+                catch (Exception ex)
+                {
+                    sMessage = "Error unexpected! Message Exception[" + ex.Message + "]";
+                }
+                return objQuestion;
+            }
+            public QuestionClass GetCurrentQuestion(out string sMessage)
+            {
+                sMessage = string.Empty;
+                QuestionClass objQuestion = new QuestionClass();
+                try
+                {
+                    objQuestion = ListQuestion[iIndexQuestion];
+                }
+                catch (Exception ex)
+                {
+                    sMessage = "Erro unexpected! Exception[" + ex.Message + "]";
+                }
+                return objQuestion;
+            }
+        }
+
+        class QuestionClass
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public List<Option> ListOption { get; set; }
+
+            public QuestionClass()
+            {
+                this.Id = 0;
+                this.Description = string.Empty;
+                this.ListOption = new List<Option>();
+            }
+
+            public void RandomOption(out string sMessage)
+            {
+                sMessage = string.Empty;
+                try
+                {
+                    Dictionary<int, Option> dcListOptions = new Dictionary<int, Option>();
+                    List<int> ListReaded = new List<int>();
+
+                    int iIndex, iCountOption = 0;
+                    foreach (Option pOption in this.ListOption)
+                    {
+                        dcListOptions.Add(iCountOption++, pOption);
+                    }
+
+                    Random rdFirst = new Random();
+                    this.ListOption = new List<Option>();
+
+                    for (int i = 0; i < iCountOption; i++)
+                    {
+                        do
+                        {
+                            iIndex = rdFirst.Next(iCountOption);
+
+                        } while (ListReaded.Contains(iIndex));
+
+                        ListReaded.Add(iIndex);
+
+                        this.ListOption.Add(dcListOptions[iIndex]);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    sMessage = "Error unespected! Ex[" + ex.Message + "]";
+                }
+            }
+        }
+
+        class Option
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public bool Cheecked { get; set; }
+
+            public Option()
+            {
+                this.Id = 0;
+                this.Description = string.Empty;
+                this.Cheecked = false;
+            }
+        }
+
+
+    }
+}
